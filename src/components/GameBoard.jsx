3ef180203ce838e55d514047899fbbd1e04c8f3f
HelloReact <640101030029@dpu.ac.th>
2024-08-05 15:30:50 +0700

    Rendering Multi-Dimensional Lists

---
+++ src/components/GameBoard.jsx
@@ -0,0 +1,23 @@
+const InitialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
+export default function GameBoard() {
+  return (
+    <ol id="game-board">
+      {InitialGameBoard.map((row, rowIndex) => (
+        <li key={rowIndex}>
+          <ol>
+            {row.map((playerSymbol ,colIndex) => (
+              <li key={colIndex}>
+                <button>{playerSymbol}</button>
+              </li>
+            ))}
+          </ol>
+        </li>
+      ))}
+    </ol>
+  );
+}
